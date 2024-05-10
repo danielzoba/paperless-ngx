ee364d40221dd88a5c819654c30504fa89de0b00
Daniel Sobe <hardcoredaniel@seznam.cz>
2024-05-10 14:56:38 +0200

    fix specification of platform

--- build_tag_container.sh
+++ build_tag_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker buildx build --platform linux/arm32v7 -t paperless-ngx .
+docker buildx build --platform linux/arm/v7 -t paperless-ngx .
 
 docker image tag paperless-ngx:latest hcdaniel/paperless-ngx:v2.0.0
 
